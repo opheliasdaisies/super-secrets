fb2566d0387e40db5f333edd8f9377af698b77fc
Sara Gorecki <sgorecki@gmail.com>
2020-04-15 14:15:40 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Secret = ({ secret }) => <div className='secret'>{secret.text}</div>
+
+function SecretForm({ addSecret }) {
+  const [secretTitle, setSecretTitle] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDEfault();
+    if (!secretTitle) return;
+    addSecret(secretTitle);
+    setSecretTitle('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        className='input'
+        value={secretTitle}
+        onChange={e => setSecretTitle(e.target.value)}
+      />
+    </form>
+  )
+}
+
+function App() {
+  const [secrets, setSecrets] = useState([
+    {text: 'Learn about React'},
+    {text: 'meet friend for lunch'},
+    {text: 'build really cool app'}
+  ]);
+
+  const addSecret = newSecret => {
+    const updatedSecrets = [...secrets, { newSecret }]
+    setSecrets(updatedSecrets);
+  };
+
+  return (
+    <div className='app'>
+      <div className='secret-list'>
+        {secrets.map((secret, index) => (
+          <Secret
+            key={index}
+            index={index}
+            secret={secret}
+          />
+        ))}
+        <SecretForm addSecret={addSecret} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;

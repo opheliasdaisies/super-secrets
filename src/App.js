a4ea97017ee1838c0627bc1c6dbf93765084bf7b
Sara Gorecki <sgorecki@gmail.com>
2020-04-16 18:24:00 -0400

    Finished up modified todo list tutorial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const Secret = ({ secret }) => <div className='secret'>{secret.text}</div>
+function Secret({ secret, index, findSecret, removeSecret }) {
+  return(
+    <div
+      className='secret'
+      style={{ textDecoration: secret.isCompleted ? "line-through" : '' }}
+    >
+      {secret.text}
+      <div>
+        <button onClick={() => findSecret(index)}>Find</button>
+        <button onClick={() => removeSecret(index)}>X</button>
+      </div>
+    </div>
+  )
+}
 
 function SecretForm({ addSecret }) {
   const [secretTitle, setSecretTitle] = useState('');
 
   const handleSubmit = e => {
-    e.preventDEfault();
+    e.preventDefault();
     if (!secretTitle) return;
     addSecret(secretTitle);
     setSecretTitle('');
@@ -28,15 +40,36 @@ function SecretForm({ addSecret }) {
 
 function App() {
   const [secrets, setSecrets] = useState([
-    {text: 'Learn about React'},
-    {text: 'meet friend for lunch'},
-    {text: 'build really cool app'}
+    {
+      text: 'Learn about React',
+      isCompleted: false
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false
+    },
+    {
+      text: 'build really cool app',
+      isCompleted: false
+    }
   ]);
 
-  const addSecret = newSecret => {
-    const updatedSecrets = [...secrets, { newSecret }]
+  const addSecret = text => {
+    const updatedSecrets = [...secrets, { text }]
     setSecrets(updatedSecrets);
   };
+
+  const findSecret = index => {
+    const newSecrets = [...secrets];
+    newSecrets[index].isCompleted = true;
+    setSecrets(newSecrets);
+  }
+
+  const removeSecret = index => {
+    const newSecrets = [...secrets];
+    newSecrets.splice(index, 1);
+    setSecrets(newSecrets);
+  }
 
   return (
     <div className='app'>
@@ -46,6 +79,8 @@ function App() {
             key={index}
             index={index}
             secret={secret}
+            findSecret={findSecret}
+            removeSecret={removeSecret}
           />
         ))}
         <SecretForm addSecret={addSecret} />
